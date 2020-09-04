a6da07cdb950130e36d1d79e65c781a29eecf09f
Marcelo Ratton <rattones@marcelorattoncom>
2020-09-04 12:33:40 -0300

    configurando acesso a base de dados

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -31,10 +31,10 @@ class Database extends \CodeIgniter\Database\Config
 	 */
 	public $default = [
 		'DSN'      => '',
-		'hostname' => 'localhost',
-		'username' => '',
-		'password' => '',
-		'database' => '',
+		'hostname' => '192.168.0.150',
+		'username' => 'owteste',
+		'password' => '123456Ow',
+		'database' => 'owmovimentacoes',
 		'DBDriver' => 'MySQLi',
 		'DBPrefix' => '',
 		'pConnect' => false,
@@ -48,7 +48,7 @@ class Database extends \CodeIgniter\Database\Config
 		'compress' => false,
 		'strictOn' => false,
 		'failover' => [],
-		'port'     => 3306,
+		'port'     => 3308,
 	];
 
 	/**
